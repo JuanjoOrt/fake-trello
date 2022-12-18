73f73754c40c5823b72c4cbb0810d077b1821e95
Juanjo Ortiz <juanjoseortizgamez@gmail.com>
2022-12-18 19:58:30 +0100

    fixing styles

--- src/Table/RowItem.jsx
+++ src/Table/RowItem.jsx
@@ -5,9 +5,10 @@ const TableRowItem = styled.div`
   width: ${({width}) => `${width}%`};
   padding: 5px 5px;
   border-top: 1px solid black;
+  background: ${({isDragging}) => isDragging && `rgba(29, 29, 37, 0.43)`};
 `
 
-export default function RowItem ({ width, row, position }) {
+export default function RowItem ({ width, row, position, isDragging }) {
   const value = row[data.columns[position]]
-  return <TableRowItem width={width}> {value ? value : ''} </TableRowItem>
+  return <TableRowItem width={width} isDragging={isDragging}> {value ? value : ''} </TableRowItem>
 }
--- src/Table/index.jsx
+++ src/Table/index.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-import data from "./tabledata.json"
+import dataBack from "./tabledata.json"
 import RowItem from "./RowItem.jsx";
 import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import {useState} from "react";
 
 const TableContainer = styled.div`
   max-width: 700px;
@@ -34,10 +35,24 @@ const TableRow = styled.div`
 
 
 export default function Table () {
+  const [data, setData] = useState(dataBack)
   const width = 100 / data.columns.length
 
+  const handleOnDragEnd = (result) => {
+    const { source, destination } = result
+
+    const newRows = Array.from(data.rows)
+    const rowMoved = newRows.splice(source.index, 1)[0]
+    newRows.splice(destination.index, 0, rowMoved)
+
+    setData({
+      ...data,
+      rows: newRows
+    })
+  }
+
   return (
-    <DragDropContext onDragEnd={(result) => console.log(result)}>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
       <TableContainer>
         <TableHeader>
           { data.columns.map( headerItem => <HeaderItem key={headerItem} width={width}>{headerItem}</HeaderItem> ) }
@@ -47,13 +62,13 @@ export default function Table () {
             <div ref={provided.innerRef} {...provided.droppableProps}>
               { data.rows.map( (row, index) => (
                 <Draggable key={row.id} draggableId={`${row.id}`} index={index}>
-                  { (provided) => (
+                  { (provided, snapshot) => (
                   <TableRow {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}>
-                    <RowItem width={width} row={row} position={0} index={index}/>
-                    <RowItem width={width} row={row} position={1} index={index} />
-                    <RowItem width={width} row={row} position={2} index={index} />
-                    <RowItem width={width} row={row} position={3} index={index} />
-                    <RowItem width={width} row={row} position={4} index={index} />
+                    <RowItem width={width} row={row} position={0} index={index} isDragging={snapshot.isDragging}/>
+                    <RowItem width={width} row={row} position={1} index={index} isDragging={snapshot.isDragging}/>
+                    <RowItem width={width} row={row} position={2} index={index} isDragging={snapshot.isDragging}/>
+                    <RowItem width={width} row={row} position={3} index={index} isDragging={snapshot.isDragging}/>
+                    <RowItem width={width} row={row} position={4} index={index} isDragging={snapshot.isDragging}/>
                   </TableRow>
                   )}
                 </Draggable>
